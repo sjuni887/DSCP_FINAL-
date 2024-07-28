3ae80573ca7d57afe56ab0238ee1740da26ff454
sjuni887 <130960046+sjuni887@users.noreply.github.com>
2024-07-28 21:33:00 +0800

    new dashboard from amelia
    amelia's new dashboard, you need to log in with her account to do it.

--- streamlit_app.py
+++ streamlit_app.py
@@ -396,7 +396,7 @@ def main():
 
     with tab4:
         st.header("Data Visualization")
-        power_bi_url = "https://app.powerbi.com/reportEmbed?reportId=adc0be8b-a4b0-4ec5-8fa9-ac0491135efa&autoAuth=true&ctid=cba9e115-3016-4462-a1ab-a565cba0cdf1"
+        power_bi_url = "https://app.powerbi.com/reportEmbed?reportId=7ca96589-22e3-465d-9f5c-af24869867b4&autoAuth=true&ctid=cba9e115-3016-4462-a1ab-a565cba0cdf1"
         st.markdown(f"""
             <iframe width="170%" height="700" src="{power_bi_url}" frameborder="0" allowfullscreen="true"></iframe>
         """, unsafe_allow_html=True)
