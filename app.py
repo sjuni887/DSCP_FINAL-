b118271fda6b7a9caf7b6b477ada2c4d12cb8645
sjuni887 <130960046+sjuni887@users.noreply.github.com>
2024-07-31 19:22:17 +0800

    border

--- app.py
+++ app.py
@@ -218,7 +218,7 @@ def main():
         st.header("Data Visualization")
         power_bi_url = "https://app.powerbi.com/reportEmbed?reportId=7ca96589-22e3-465d-9f5c-af24869867b4&autoAuth=true&ctid=cba9e115-3016-4462-a1ab-a565cba0cdf1"
         st.markdown(f"""
-            <iframe width="100%" height="700" src="{power_bi_url}" frameborder="0" allowfullscreen="true"></iframe>
+        <iframe width="100%" height="700" src="{power_bi_url}" frameborder="0" allowfullscreen="true" style="border-radius: 15px; border: 2px solid #ccc;"></iframe>
         """, unsafe_allow_html=True)
 
     elif st.session_state.page == "patient_data":
